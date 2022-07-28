70ec6bf24ef38f2aefab64346ce96182728a2828
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-07-28 16:18:06 +0300

    correct mistake

--- src/components/TransactionHistory/Transaction/Transaction.styled.jsx
+++ src/components/TransactionHistory/Transaction/Transaction.styled.jsx
@@ -5,6 +5,6 @@ export const TableRow = styled.td`
 
 export const TableColumn = styled.tr`
 background-color: ${props => {
-    return props.index % 2 == 0 ?'White': '#ebe8e8';
+    return props.index % 2 === 0 ?'White': '#ebe8e8';
 }}; 
 `;
